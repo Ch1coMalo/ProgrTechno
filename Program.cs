48639006a9459ed6eeaac840f78856d97d214671
Ch1coMalo <105455288+Ch1coMalo@users.noreply.github.com>
2022-11-10 08:57:34 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
-namespace HelloWrld
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
